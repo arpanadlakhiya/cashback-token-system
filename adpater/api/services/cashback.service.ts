ab17fe5f05d906a462a175e484ba5b0053bec537
diptesh82 <dipteshmandal555@gmail.com>
2023-07-23 11:17:30 +0530

    api peasted

---
+++ adpater/api/services/cashback.service.ts
@@ -0,0 +1,36 @@
+import * as batchInterface from "../../interfaces/batch.interface";
+import * as HTTPResponseUtils from "../../utils/httpResponseUtils";
+import * as batchDB from "../../utils/Database/batch.database";
+import * as db from "../../utils/Database/dbClient";
+import * as constants from "../../utils/constants";
+import * as hlfClient from "../../utils/hlfClient/hlfClient";
+import * as orgUtils from "../../utils/orgUtils";
+
+
+export const GenerateCashback = async (cashBackRequest: any) => {
+    console.log(`services GenerateCashback :: User ${JSON.stringify(cashBackRequest)}`);
+  
+    let responce;
+    return responce;
+  };
+  
+  export const ClaimCashback = async (cashBackRequest: any) => {
+    console.log(`services ClaimCashback :: User ${JSON.stringify(cashBackRequest)}`);
+  
+    let responce;
+    return responce;
+  };
+  export const CreateCashbackToken = async (cashBackRequest: any) => {
+    console.log(`services CreateCashbackToken :: User ${JSON.stringify(cashBackRequest)}`);
+  
+    let responce;
+    return responce;
+  };
+  
+  export const QueryCashbackToken = async (cashBackRequest: any) => {
+    console.log(`services QueryCashbackToken :: User ${JSON.stringify(cashBackRequest)}`);
+  
+    let responce;
+    return responce;
+  };
+  
