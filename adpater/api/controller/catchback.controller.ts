7275cece43b5dc95154c268c618266b21c664501
diptesh82 <dipteshmandal555@gmail.com>
2023-07-23 10:35:54 +0530

    cashback api

---
+++ adpater/api/controller/catchback.controller.ts
@@ -0,0 +1,5 @@
+import * as HTTPResponseUtil from "../../utils/httpResponseUtils";
+import * as userInterface from "../../interfaces/user.interface";
+import * as batchInterface from "../../interfaces/batch.interface";
+import * as batchService from "../services/batch.service";
+import * as constants from "../../utils/constants";
