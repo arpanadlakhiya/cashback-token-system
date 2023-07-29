5577800f5483dd240b8d68a904d3611340ed8b35
diptesh82 <dipteshmandal555@gmail.com>
2023-07-29 13:21:27 +0530

    added api

--- adpater/src/api/services/ruleset.service.ts
+++ adpater/src/api/services/ruleset.service.ts
@@ -1,10 +1,11 @@
-import * as userInterface from "../../interfaces/user.interface";
+import * as rulesetInterface from "../../interfaces/ruleset.interface";
 import * as HTTPResponseUtils from "../../utils/httpResponseUtils";
+import * as dlt from "../../utils/hlfClient/hlfClient"
 
 
-export const ruleSet = async (rulesetDetails: userInterface.User) => {
+export const ruleSet = async (rulesetDetails: rulesetInterface.ruleSet ) => {
   try {
-   
+   await dlt.invoke
   } catch (err) {
     console.log(
       `Service register :: Failed to register user with Error: ${err}`
